b018775f3bdd7982cba39bcf1297bc4a7f02403a
gnithin <nithin.linkin@gmail.com>
2016-08-02 15:05:05 +0530

    Add a workable gridview

--- app/controllers/movieGrid.js
+++ app/controllers/movieGrid.js
@@ -1,12 +1,14 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
 
-var the_image = args.image || '';
-var item_width = args.width || '95%';
-var item_height = args.height || '95%';
+var poster_url = args.Poster || '';
+var movie_title = args.Title || 'Default Title';
+var movie_year = args.Year || "";
 
-$.thumb.image = 'http://mobilemarketingwatch.com/wp-content/uploads/2016/01/Is-Google-Searching-for-the-Next-Big-Thing1.jpg';
+$.thumb.image = poster_url;
 $.thumb.defaultImage = '/images/Default.png';
+if(movie_year !== ""){
+	$.releaseYear.text = "Released on : " + movie_year;	
+}
 
-$.thumb.width = item_width;
-$.thumb.height = item_height;	
+$.thumbTitle.text = movie_title;
--- app/controllers/movie_details.js
+++ app/controllers/movie_details.js
@@ -1,45 +1,12 @@
-$.fg.init({
-    columns:3,
-    space:5,
-    gridBackgroundColor:'#fff',
-    itemHeightDelta: 0,
-    itemBackgroundColor:'#eee',
-    itemBorderColor:'transparent',
-    itemBorderWidth:0,
-    itemBorderRadius:0
-});
-
-function addGrid(){
+function addGrid(args){
 	var items = [];
-	var image_url="";
-	var sample_data = [
-	        {title:'sample 1', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 2', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 3', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 4', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 5', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 6', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 7', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 8', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 9', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 10', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 11', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 12', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 13', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 14', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'},
-	        {title:'sample 15', image:'http://lh3.googleusercontent.com/-Y_J3VCQOgrI/AAAAAAAAAAI/AAAAAAAAAAA/IgpyZta5ccw/photo.jpg'}
-	    ];
 	
-	for (var x=0; x < sample_data.length; x++){
+	for (var x=0; x < args.length; x++){
+		//THIS IS THE DATA THAT WE WANT AVAILABLE FOR THIS ITEM WHEN onItemClick OCCURS
+	    var values = args[x];
 	
 	    //CREATES A VIEW WITH OUR CUSTOM LAYOUT
-	    var view = Alloy.createController('movieGrid').getView();
-	
-	    //THIS IS THE DATA THAT WE WANT AVAILABLE FOR THIS ITEM WHEN onItemClick OCCURS
-	    var values = {
-	        title: sample_data[x].title,
-	        image: sample_data[x].image
-	    };
+	    var view = Alloy.createController('movieGrid', values).getView();
 	
 	    //NOW WE PUSH TO THE ARRAY THE VIEW AND THE DATA
 	    items.push({
@@ -51,16 +18,6 @@ function addGrid(){
 	//ADD ALL THE ITEMS TO THE GRID
 	$.fg.addGridItems(items);
 }
-
-
-
-(function(){
-	// Arguments passed into this controller can be accessed via the `$.args` object directly or:
-	var args = $.args;
-	//buildTableView(args);
-	alert("Hello there mate!");
-	addGrid();
-})();
 
 // build the ui from here.
 function buildTableView(args){
@@ -75,3 +32,20 @@ function buildTableView(args){
 	row.add(view);
 	$.movieDetailsTable.push(row);
 }
+
+(function(){
+	var args = $.args;
+	
+	$.fg.init({
+	    columns:2,
+	    space:0,
+	    gridBackgroundColor:'#fff',
+	    itemHeightDelta: 0,
+	    itemBackgroundColor:'#eee',
+	    itemBorderColor:'transparent',
+	    itemBorderWidth:0,
+	    itemBorderRadius:0
+	});
+	
+	addGrid(args);
+})();
