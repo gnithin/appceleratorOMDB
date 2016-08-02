61242220583936f34a550c17de46a6fd226b8450
gnithin <nithin.linkin@gmail.com>
2016-08-02 16:50:40 +0530

    Clean up code in movieGrid

--- app/controllers/movieGrid.js
+++ app/controllers/movieGrid.js
@@ -1,14 +1,15 @@
-// Arguments passed into this controller can be accessed via the `$.args` object directly or:
-var args = $.args;
-
-var poster_url = args.Poster || '';
-var movie_title = args.Title || 'Default Title';
-var movie_year = args.Year || "";
-
-$.thumb.image = poster_url;
-$.thumb.defaultImage = '/images/Default.png';
-if(movie_year !== ""){
-	$.releaseYear.text = "Released on : " + movie_year;	
-}
-
-$.thumbTitle.text = movie_title;
+(function(){
+	var args = $.args;
+	
+	var poster_url = args.Poster || '';
+	var movie_title = args.Title || 'Default Title';
+	var movie_year = args.Year || "";
+	
+	$.thumb.image = poster_url;
+	$.thumb.defaultImage = '/images/Default.png';
+	if(movie_year !== ""){
+		$.releaseYear.text = "Released on : " + movie_year;	
+	}
+	
+	$.thumbTitle.text = movie_title;	
+})();
